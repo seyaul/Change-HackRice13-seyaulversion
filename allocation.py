ccbfc4473f8ce622cbea31a9c98ed0e8e2048a55
Gavin <gbd2@rice.edu>
2023-09-23 14:42:59 -0500

    Creation of analysis file

--- allocation.py
+++ allocation.py
@@ -14,10 +14,10 @@ old_default = np.array([80, 0, 20, 0])
 to_interpolate = [young_default, mid_age_default, old_default]
 ages = [18,42,65]
 
-high_risk_LT = np.array([0, 10, 20, 70])
-high_risk_ST = np.array([50, 0, 0, 50])
-low_risk_LT = np.array([0, 70, 25, 5])
-low_risk_ST = np.array([80, 0, 20, 0])
+high_risk_LT = np.array([0, 10, 20, 70])/100
+high_risk_ST = np.array([50, 0, 0, 50])/100
+low_risk_LT = np.array([0, 70, 25, 5])/100
+low_risk_ST = np.array([80, 0, 20, 0])/100
     
 def interpolate_plan(vectors, positions, target_position):
     # Find the two nearest positions to the target position
@@ -38,24 +38,23 @@ def interpolate_plan(vectors, positions, target_position):
 
 
 def allocate(balance, deposit, user):
-    if(user["Plan"] == "Default"):
-        if balance["Liquid"] < user["Salary"] / 2:
-            needed = user["Salary"] / 2 - balance["Liquid"]
-            if deposit < needed:
-                balance["Liquid"] += deposit
-                return balance
-            else:
-                balance["Liquid"] += needed
-                deposit -= needed
+    if balance["Liquid"] < user["Salary"] / 2:
+        needed = user["Salary"] / 2 - balance["Liquid"]
+        if deposit < needed:
+            balance["Liquid"] += deposit
+            return balance
+        else:
+            balance["Liquid"] += needed
+            deposit -= needed
 
-        if balance["CurrRetirement"] < 21500:
-            if deposit < 21500 - balance["CurrRetirement"]:
-                balance["CurrRetirement"] += deposit
-                return balance
-            else:
-                deposit -= (21500 - balance["CurrRetirement"])
-                balance["CurrRetirement"] = 21500
-
+    if balance["CurrRetirement"] < 21500:
+        if deposit < 21500 - balance["CurrRetirement"]:
+            balance["CurrRetirement"] += deposit
+            return balance
+        else:
+            deposit -= (21500 - balance["CurrRetirement"])
+            balance["CurrRetirement"] = 21500
+    if(user["Plan"] == "Default"):    
         interpolated_plan = interpolate_plan(to_interpolate, ages, user["Age"]) / 100
         balance["ST Fixed Income"] += interpolated_plan[0]*deposit
         balance["LT Fixed Income"] += interpolated_plan[1]*deposit
@@ -89,3 +88,9 @@ def allocate(balance, deposit, user):
         balance["Tech"] += low_risk_ST[3]*deposit
 
     return balance
+
+balance = {"Liquid": 0, "ST Fixed Income": 0, "LT Fixed Income": 0, "ETF": 0, "Tech": 0, "CurrRetirement": 0}
+user = {"Salary": 250000, "Age": 24, "Plan": "High Risk Short Term"}
+
+testres = allocate(balance, 250000, user)
+print(testres, sum(testres.values()))
---
+++ analysis.py
@@ -0,0 +1,20 @@
+import allocation
+import numpy as np
+import pandas as pd
+
+MEAN_COL = 3018
+print(allocation.balance)
+
+def monthly_deposit_withdraw(user):
+    monthly_withdraw = MEAN_COL + 250 *np.random.standard_normal()
+    monthly_deposit = user["Salary"]/12
+    return monthly_withdraw, monthly_deposit
+
+def compound_allocate(balance, user):
+    monthly_withdraw, monthly_deposit = monthly_deposit_withdraw(user)
+    allocation.balance["Liquid"] -= monthly_withdraw
+    allocation.allocate(allocation.balance, monthly_deposit, user)
+
+    return allocation.balance
+
+print(compound_allocate(allocation.balance, allocation.user))
